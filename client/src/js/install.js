b39a98da80dfb9355230c41e84fc4e93524f53e6
Thomas James <tomsterlingjames@outlook.com.au>
2022-04-28 10:45:51 +1000

    adjusted a few things to make it run correctly

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,24 +1,23 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
-    window.deferredPrompt = event;
+window.addEventListener("beforeinstallprompt", (event) => {
+  window.deferredPrompt = event;
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-installBtn.addEventListener('click', () => {
-    const promptEvent = window.deferredPrompt;
-    if (!promptEvent) {
-     return;
-    }
-    promptEvent.prompt();
-    window.deferredPrompt = null;
-  });
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    return;
+  }
 
+  promptEvent.prompt();
+  window.deferredPrompt = null;
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {
-    window.deferredPrompt = null;
-  });
-  
+window.addEventListener("appinstalled", (event) => {
+  window.deferredPrompt = null;
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,21 +1,21 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
@@ -30,7 +30,7 @@ module.exports = () => {
         inject: true,
         fingerprints: false,
         name: "Just Another Text Editor",
-        short_name: "J.A.T.E",
+        short_name: "JATE",
         description: "Takes notes with JavaScript syntax highlighting!",
         display: "standalone",
         orientation: "portrait",
@@ -56,15 +56,16 @@ module.exports = () => {
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
+          type: "asset/resource",
         },
         {
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"],
             },
           },
         },
